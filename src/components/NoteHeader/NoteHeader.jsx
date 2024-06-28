952d7f6bd6801262f31c9704f35062cd52a5eace
Atmaram Kambli <118362436+atmaram-kambli@users.noreply.github.com>
2024-06-28 12:07:00 +0530

    Update NoteHeader.jsx

--- src/components/NoteHeader/NoteHeader.jsx
+++ src/components/NoteHeader/NoteHeader.jsx
@@ -16,7 +16,7 @@ const NoteHeader = ({handleGrid, grid}) => {
           <input type="text" placeholder='Search...' title='Search feature is under testing' disabled/>
         </div>
         <div className="right px-2">
-            <button className='btn btn-light mx-0' onClick={() => {window.location.reload();}} title='Refresh'><i className="fa-solid fa-arrow-rotate-right"></i></button>
+{/*             <button className='btn btn-light mx-0' onClick={() => {window.location.reload();}} title='Refresh'><i className="fa-solid fa-arrow-rotate-right"></i></button> */}
             {grid === 'list' && <button className='btn btn-light mx-0' onClick={handleGrid} title='List View'><i className="fa-solid fa-list"></i></button>}
             {grid !== 'list' && <button className='btn btn-light mx-0' onClick={handleGrid} title='Grid View'><i className="fa-solid fa-list"></i></button>}
             <button className='btn btn-light mx-0' title='Settings'><i className="fa-solid fa-gear"></i></button>
